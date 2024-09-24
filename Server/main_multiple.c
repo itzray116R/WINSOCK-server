d2ba2d724e6a71570535fa0bf2999735c0f97fae
itzray116R <rayhaanyaser@gmail.com>
2024-09-24 17:15:54 -0400

    Update main_multiple.c

--- Server/main_multiple.c
+++ Server/main_multiple.c
@@ -13,7 +13,8 @@
 #define BUFLEN 512
 #define PORT 27015
 #define ADDRESS "127.0.0.1" //aka localhost
-#define MAX_CLIENTS 30
+#define MAX_CLIENTS 10
+
 int main() {
     printf("Hello World!\n");
 
@@ -78,27 +79,29 @@ int main() {
     // Main Loop ==============================================================
     
     //variables
-    fd_set socketSet; //set of active clients
-    SOCKET clients[MAX_CLIENTS]; //array of clients
-    SOCKET sd, max_sd; //current socket
-    int curNoClients = 0; //info about the array
+    fd_set socketSet;                   //set of active clients
+    SOCKET clients[MAX_CLIENTS];        //array of clients
+    int curNoClients = 0;               // active slots in the array
+    SOCKET sd, max_sd;                  // placeholders
     struct sockaddr_in clientAddr;
     int clientAddrlen;
-    char quit = 0;
+    char running = !0;  //server state
 
     char recvbuf[BUFLEN];
 
-
-    char *welcome = "Welcome to the server";
+    char *welcome = "Welcome to the server :)\n";
     int welcomeLength = strlen(welcome);
-    char *goodbye = "Goodbye!";
+    char *full = "Sorry The server is full :(\n";
+    int fullLength = strlen(full);
+    char *goodbye = "Goodbye! \n";
     int goodbyeLength = strlen(goodbye);
 
     //clear a client array
     memset(clients, 0, MAX_CLIENTS * sizeof(SOCKET));
 
-    while(!quit) {
-        //clear the set
+    while(running) {
+
+        //clear the Socket set
         FD_ZERO(&socketSet);
 
         //add listener socket
@@ -122,14 +125,13 @@ int main() {
             continue;
         }
 
-        //determine which sockets have activity
+        //determine which listener have activity
         if (FD_ISSET(listener, &socketSet)){
 
             // accept connection
             sd = accept(listener, NULL, NULL);
             if (sd == INVALID_SOCKET) {
                 printf("ERROR accepting failed: %d\n", WSAGetLastError());
-                closesocket(listener);
             }
 
             //get client info
@@ -141,74 +143,83 @@ int main() {
                 ntohs(clientAddr.sin_port)
                 );
 
-            //send welcome message
-            sendRes = send(sd, welcome, welcomeLength, 0);
-            if (sendRes != welcomeLength) {
-                printf("send failed: %d\n", WSAGetLastError());
-                shutdown(sd, SD_BOTH);
-                closesocket(sd);
-            }
 
             //add new socket to the array
             if (curNoClients >= MAX_CLIENTS) {
-                printf("Too many clients\n");
+                printf("Full\n");
+                sendRes = send(sd, full, fullLength, 0);
+                if (sendRes != fullLength) {
+                    printf("Error sending: %d\n", WSAGetLastError());
+                }
+
+
                 shutdown(sd, SD_BOTH);
                 closesocket(sd);
             } else {
-                for (int i = 0; i < MAX_CLIENTS; i++) {
-                    if (clients[i] == 0) {
+                //scan through list
+                int i;
+                for (i = 0; i < MAX_CLIENTS; i++) {
+                    if (!clients[i]) {
                         clients[i] = sd;
                         printf("Added to the list at index %d\n", i);
                         curNoClients++;
                         break;
                     }
                 }
-            }
 
-            for (int i = 0; i <MAX_CLIENTS;i++) {
-                if (!clients[i]){
-                    continue;
+                //send welcome message
+                sendRes = send(sd, welcome, welcomeLength, 0);
+                if (sendRes != welcomeLength) {
+                    printf("send failed: %d\n", WSAGetLastError());
+                    shutdown(sd, SD_BOTH);
+                    closesocket(sd);
+                    clients[i] = 0;
+                    curNoClients--;
                 }
-                sd = clients[i];
+            }
+        }
 
-                if (FD_ISSET(sd, &socketSet)){
-                    //get message
-                    res = recv(sd, recvbuf, BUFLEN, 0);
-                    if (res > 0 ) {
-                        //print message
-                        recvbuf[res] = '\0';
-                        printf("Message(%d): %s\n", res, recvbuf);
-
-                        //test if quit command
-                        if (!memcmp(recvbuf, "/quit", 5 * sizeof(char))) {
-                            quit = !0; //quit true
-                            printf("Client has disconnected\n");
-                            break;
-                        }
-
-                        //echo message
-                        sendRes = send(sd, recvbuf, res, 0);
-                        if (sendRes  == SOCKET_ERROR) {
-                            printf("ECHO failed: %d\n", WSAGetLastError());
-                            shutdown(sd, SD_BOTH);
-                            closesocket(sd);
-                            clients[i] = 0;
-                            curNoClients--;
-                        }
-                    } else {
-                        //get client information
-                        getpeername(sd, (struct sockaddr*)&clientAddr, &clientAddrlen);
-                        printf(
-                            "Client disconnected at %s:%d\n",
-                            sd,
-                            inet_ntoa(clientAddr.sin_addr),
-                            ntohs(clientAddr.sin_port)
-                            );
+        //iterate through the clients
+        for (int i = 0; i <MAX_CLIENTS;i++) {
+            if (!clients[i]){
+                continue;
+            }
+            sd = clients[i];
+            // determine if client has activity
+            if (FD_ISSET(sd, &socketSet)){
+                //get message
+                res = recv(sd, recvbuf, BUFLEN, 0);
+                if (res > 0 ) {
+                    //print message
+                    recvbuf[res] = '\0';
+                    printf("Recieved (%d): %s\n", res, recvbuf);
+                    //test if quit command
+                    if (!memcmp(recvbuf, "/quit", 5 * sizeof(char))) {
+                        running = 0; //running false
+                        break;
+                    }
+                    //echo message
+                    sendRes = send(sd, recvbuf, res, 0);
+                    if (sendRes  == SOCKET_ERROR) {
+                        printf("ECHO failed: %d\n", WSAGetLastError());
                         shutdown(sd, SD_BOTH);
                         closesocket(sd);
                         clients[i] = 0;
                         curNoClients--;
                     }
+                } else {
+                    //get client information
+                    getpeername(sd, (struct sockaddr*)&clientAddr, &clientAddrlen);
+                    printf(
+                        "Client disconnected at %s:%d\n",
+                        sd,
+                        inet_ntoa(clientAddr.sin_addr),
+                        ntohs(clientAddr.sin_port)
+                        );
+                    shutdown(sd, SD_BOTH);
+                    closesocket(sd);
+                    clients[i] = 0;
+                    curNoClients--;
                 }
             }
         }
